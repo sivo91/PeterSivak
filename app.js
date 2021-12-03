0b50b66ce1c239bab21b9a987f097399c9e33b05
Peter <p.sivak91@gmail.com>
2021-12-03 11:56:47 -0800

    opravi

---
+++ app.js
@@ -0,0 +1,568 @@
+ 
+
+      //DRESY & LOGO
+    const jersy = [
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/lm.png",
+        id:1
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/pn.png",
+        id:2
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/povazska.png",
+        id:3
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/dubnica.png",
+        id:4
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/tn.png",
+        id:5
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/za.png",
+        id:6
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/HKM.png",
+        id:7
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/psg-zlin.png",
+        id:8
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/sf.png",
+        id:9
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/shark.png",
+        id:10
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/ak.png",
+        id:11
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/abb.png",
+        id:12
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/stockton.png",
+        id:13
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/brifge.png",
+        id:14
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/ORLANDO_SOLAR_BEARS.png",
+        id:15
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/utah.png",
+        id:16
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/komets.png",
+        id:17
+      },
+      {
+      imagejersy: "../logos/dress.png",
+      imagelogo: "../logos/rapid.png",
+      id:18
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/tulsa.png",
+        id:19
+      },
+      {
+        imagejersy: "../logos/dress.png",
+        imagelogo: "../logos/sd.png",
+        id:20
+      },
+    ] 
+      
+      // data arr obj stats
+    const datastats = [
+      {
+        id: 1,
+        Country: 'Slovakia, EU',
+        Team: "HK 32 Liptovsky Mikulas",
+        League: 'Extraliga',
+        Years: '1999-2000',
+        Seassons : 1,
+        Games: 36,
+        Goals:0,
+        Assists:0,
+        Points: 0, 
+        img: '../hokejfoto/aces.png'
+      },
+      {
+        id: 4,
+        Country: 'Slovakia, EU',
+        Team: "HK Dubnica",
+        League: 'SHL',
+        Years: '2001/2002',
+        Seassons : 2,
+        Games: 34,
+        Goals:6,
+        Assists:4,
+        Points: 10,
+        img: '../hokejfoto/DUBNICA.png'
+      },
+      {
+        id: 3,
+        Country: 'Slovakia, EU',
+        Team: "HK 95 Povazska Bystrica",
+        League: 'SHL',
+        Years: '2006-2007',
+        Seassons : 1,
+        Games: 1,
+        Goals:2,
+        Assists:0,
+        Points: 2,
+        img: '../hokejfoto/rapid.png'
+      },
+      {
+        id: 2,
+        Country: 'Slovakia, EU',
+        Team: "SHK 37 Piestany",
+        League: 'SHL',
+        Years: '2004-2005',
+        Seassons : 1,
+        Games: 3,
+        Goals:0,
+        Assists:0,
+        Points: 0,
+        img: '../hokejfoto/ALLSTAR.png'
+      },
+      {
+        id: 5,
+        Country: 'Slovakia, EU',
+        Team: "HK Dukla Trencin",
+        League: 'Extraliga',
+        Years: '2001-2007',
+        Seassons : 6,
+        Games: 254,
+        Goals:39,
+        Assists:33,
+        Points: 72,
+        img: '../hokejfoto/tn.png'
+      },
+      {
+        id:6,
+        Country: 'Slovakia, EU',
+        Team: "MsHK Zilina",
+        League: 'Extraliga',
+        Years: '07-09, 11-12',
+        Seassons : 3,
+        Games: 162,
+        Goals:60,
+        Assists:64,
+        Points: 124,
+        img: '../hokejfoto/za.png'
+      },
+      {
+        id: 7,
+        Country: 'Slovakia, EU',
+        Team: "HKM Zvolen",
+        League: 'Extraliga',
+        Years: '2008-2009',
+        Seassons : 1,
+        Games: 21,
+        Goals:6,
+        Assists:6,
+        Points: 12,
+        img: '../hokejfoto/alaska.png'
+      },
+      {
+        id: 8,
+        Country: 'Czech Republic, EU',
+        Team: "PSG Zlin",
+        League: 'Extraliga',
+        Years: '2009-2011',
+        Seassons : 2,
+        Games: 107,
+        Goals:4,
+        Assists:18,
+        Points: 22,
+        img: '../hokejfoto/zlin.png'
+      },
+      {
+        id: 9,
+        Country: 'California, USA',
+        Team: "San Francisco Bulls",
+        League: 'ECHL',
+        Years: '2012-2013',
+        Seassons : 1,
+        Games: 67,
+        Goals:34,
+        Assists:25,
+        Points: 59,
+        img: '../hokejfoto/sanfran.png'
+      },
+      {
+        id: 15,
+        Country: 'USA / Florida',
+        Team: "Orlando Solar Bears",
+        League: 'ECHL',
+        Years: '2014-2015',
+        Seassons : 1,
+        Games: 25,
+        Goals:5,
+        Assists:15,
+        Points: 20,
+        img: '../hokejfoto/orlando.png'
+      },
+      {
+        id: 10,
+        Country: 'Massachusetts, USA',
+        Team: "Worchester Sharks",
+        League: 'AHL',
+        Years: '2012-2013',
+        Seassons : 1,
+        Games: 9,
+        Goals:4,
+        Assists:1,
+        Points: 5,
+        img: '../hokejfoto/sharks.png'
+      },
+      {
+        id: 11,
+        Country: 'Alaska, USA',
+        Team: "Alaska Aces",
+        League: 'ECHL',
+        Years: '13-14, 15-17',
+        Seassons : 3,
+        Games: 181,
+        Goals:91,
+        Assists:121,
+        Points: 212,
+        img: '../hokejfoto/alaskacupp.png'
+      },
+      {
+        id: 12,
+        Country: 'Canada',
+        Team: "Abbotsford Heat",
+        League: 'AHL',
+        Years: '2013-2014',
+        Seassons : 1,
+        Games: 3,
+        Goals:0,
+        Assists:0,
+        Points: 0,
+        img: '../hokejfoto/abots.png'
+      },
+      {
+        id: 13,
+        Country: 'California, USA',
+        Team: "Stockton Thunder",
+        League: 'ECHL',
+        Years: '2014-2015',
+        Seassons : 1,
+        Games: 29,
+        Goals:10,
+        Assists:16,
+        Points: 26,
+        img: '../hokejfoto/stockton.png'
+      },
+      {  
+        id: 14,   
+        Country: 'Connecticut, USA',
+        Team: "Bridgeport Sound Tigers",
+        League: 'AHL',
+        Years: '2014-2015',
+        Seassons : 1,
+        Games: 6,
+        Goals:1,
+        Assists:0,
+        Points: 1,
+        img: '../hokejfoto/bridgeport.png'
+      },
+      {
+        id: 17,
+        Country: 'Indiana, USA',
+        Team: "Fort Wayne Komets",
+        League: 'ECHL',
+        Years: '2015-2016',
+        Seassons : 1,
+        Games: 10,
+        Goals:2,
+        Assists:6,
+        Points: 8,
+        img: '../hokejfoto/ala.png'
+      },
+      ,
+      {
+        id: 16,
+        Country: 'Utah, USA',
+        Team: "Utah Grizzlies",
+        League: 'ECHL',
+        Years: '2017-2018',
+        Seassons : 1,
+        Games: 4,
+        Goals:0,
+        Assists:1,
+        Points: 1,
+        img: '../hokejfoto/modalBG.png'
+      },
+      {
+        id: 18,
+        Country: 'South Dakota, USA',
+        Team: "Rapid City Rush",
+        League: 'ECHL',
+        Years: '2017-2018',
+        Seassons : 1,
+        Games: 58,
+        Goals:26,
+        Assists:34,
+        Points: 60,
+        img: '../hokejfoto/repid.png'
+      },
+      {
+        id: 19,
+        Country: 'Oklahoma, USA',
+        Team: "Tulsa Oilers",
+        League: 'ECHL',
+        Years: '2018-2019',
+        Seassons : 1,
+        Games: 10,
+        Goals:5,
+        Assists:3,
+        Points: 8,
+        img: '../hokejfoto/tulsa.png'
+      },
+      {
+        id: 20,
+        Country: 'California, USA',
+        Team: "San Diego Gulls",
+        League: 'AHL',
+        Years: '2018-2019',
+        Seassons : 1,
+        Games: 1,
+        Goals:0,
+        Assists:0,
+        Points: 0,
+        img: '../hokejfoto/ABBOTSF.png'
+      }
+    ]
+
+
+   // *****************************
+
+
+
+ /* toggle */
+ const homebtn = document.querySelector('.home')
+
+ function toggleClass() {
+  const navbar = document.querySelector('.navbar')
+     navbar.classList.toggle('show')
+ }
+
+
+function myFunction() {
+  let x = document.querySelector('.home')
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+ //  SHOW DRESY
+
+ const modal = document.querySelector('.modal')
+ const panel = document.querySelector('.panel')
+ 
+   // GET DRESS & LOGO
+panel.innerHTML = jersy.map(function(item) {
+ return `
+ <div class="paneldresu"> 
+
+   <!--  DRESS A LOGO -->
+     <img src="${item.imagejersy}"  class="dres" alt="dres">
+     <img src="${item.imagelogo}"  class="logoteamu" data-id="${item.id}" alt="orlando">
+</div>
+ `
+}).join('')
+
+ // DOCUMENT EVENT po natiahnuti dam even na logo
+document.addEventListener('readystatechange', (e) => {
+ initApp()
+ 
+})
+ 
+const initApp = (e) => {
+ const logoT = document.querySelectorAll('.logoteamu')
+ //console.log(logoT)
+
+   // select single logo
+   logoT.forEach(function(item) {
+   //console.log(item)
+   item.addEventListener('click', (e) => {
+    // trafi cisielko
+     //console.log(e.currentTarget.dataset.id)
+     const logoID = e.currentTarget.dataset.id
+
+
+     // SHOW MODAL & STATS & FOTOS
+     modal.innerHTML = datastats.map(function(item) {
+     if(logoID == item.id){
+       modal.classList.add('showit') 
+     return `
+     <div class="bgmodal"></div>
+      
+        <div class="textbox">
+          <h2>Country: ${item.Country}</h2>
+          <h2>Team: ${item.Team}</h2>
+          <h2>League: ${item.League}</h2>
+          <h2>Years: ${item.Years}</h2>
+          <h2>Seassons: ${item.Seassons}</h2>
+          <h2>Games: ${item.Games}</h2>
+          <h2>Goals: ${item.Goals}</h2>
+          <h2>Assists: ${item.Assists}</h2>
+          <h2>Points: ${item.Points}</h2>
+        </div>
+
+        <div class="pictbox">
+          <img src="${item.img}" class="ImgModal2" alt="lmimg">
+        </div>
+        <i class="far fa-times-circle closebtn"></i>
+     `} 
+   }).join('') // bez join commas !!!!
+
+
+  //  CLOSE MODAL 
+ const closeModal = document.querySelectorAll('.closebtn')
+
+          closeModal.forEach(function(done) {
+          //console.log(done) ok
+          done.addEventListener('click', function(e) {
+          const modal = done.parentElement
+          
+          modal.classList.remove('showit')
+          e.preventDefault()
+          })
+        }) 
+
+
+    })
+  })
+}
+
+
+
+   //   PROJECTS SECTION  ******************
+    
+  /*  FILTER PROJECT */
+  const result = document.getElementById('result')
+  const filter = document.getElementById('filter')
+  const listItems = []
+
+  gatData()
+
+  async function gatData () {
+    const res = await fetch('https://randomuser.me/api?results=50')
+
+    const data = await res.json()
+    console.log(data)
+  }
+
+   
+     
+    
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//      contact  section ---------------
+
+const addMessage = (e) => {
+  e.preventDefault()
+
+   //name validate
+  const name = document.querySelector('.firstname');
+  const last = document.querySelector('.lastName')
+  const re1 = /^[a-zA-Z]{2,10}$/
+
+   // email validate
+   const email = document.getElementById('email')
+   const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+   const relast = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+   
+   if(!re1.test(name.value) || !re.test(email.value) || !relast.test(last.value)) {
+
+    // name
+    name.classList.add('is-invalid')
+    setTimeout(function(){
+      name.classList.remove('is-invalid');
+    },3000)
+
+    //last
+    last.classList.add('is-invalid')
+    setTimeout(function(){
+      last.classList.remove('is-invalid');
+    },3000)
+    
+    // email
+    email.classList.add('is-invalid')
+     setTimeout(function(){
+      email.classList.remove('is-invalid');
+    },3000)
+
+    email.value = ''
+    name.value = ''
+    last.value = ''
+
+  } 
+}
+
+
+const clearInputs = () => {
+    document.querySelector('firstname').value = '',
+    document.querySelector('lastname').value = '',
+    document.getElementById('email').value = '',
+    document.querySelector('textArea').value = ''
+  }
+
+
+
+
+  
+
